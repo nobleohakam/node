1208435e692b6a686bb8ef7b373b418f90011be9
Noble Ohakam <63134458+nobleohakam@users.noreply.github.com>
2021-04-24 16:58:52 -0400

    Update companiesinsert.js

--- companiesinsert.js
+++ companiesinsert.js
@@ -1,37 +1,7 @@
-// const csvtojson = require("csvtojson");
-// const MongoClient = require('mongodb').MongoClient;
-// const url = "mongodb+srv://noble_oaja:asdUYT23@project1.jit3l.mongodb.net/stockticker?retryWrites=true&w=majority";
-
-// csvtojson()
-//   .fromFile("companies.csv")
-//   .then(csvData => {
-//     console.log(csvData);
-
-//   MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
-//   if(err) { return console.log(err); return;}
-//
-//     var dbo = db.db("stockticker");
-// 	var collection = dbo.collection('companies');
-//
-//     // collection.insertMany(csvData, function(err, res) {
-//     //   if (err){
-//     //       return console.log(err);
-//     //   }
-//     //   console.log("Number of documents inserted: " + res.insertedCount);
-//     //   db.close();
-//     // });
-//
-// 	console.log("Success!");
-// 	db.close();
-//
-// });
-
-
-
 //I used the module csvtojson to read the csv data as a json and the fromFile() function return the JSON-like array.
 const mongodb = require("mongodb").MongoClient;
 const csvtojson = require("csvtojson");
-
+var port = procress.env.PORT || 3000;
 
 let url = "mongodb+srv://noble_oaja:asdUYT23@project1.jit3l.mongodb.net/stockticker?retryWrites=true&w=majority";
 
@@ -51,6 +21,7 @@ csvtojson()
             console.log(`Inserted: ${res.insertedCount} rows`);
             db.close();
           });
-      }
+      }.listen(port);
     );
-  });
+});
+
